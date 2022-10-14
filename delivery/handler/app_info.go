d07da8ff40869116d28cc1af7f9f0bf0000f8c84
azmi17 <azmifarhan80@gmail.com>
2022-10-14 15:28:17 +0700

    Revision: App Info

--- delivery/handler/app_info.go
+++ delivery/handler/app_info.go
@@ -13,11 +13,11 @@ func AppInfo(ctx *gin.Context) {
 	httpio.Recv()
 
 	appInfo := map[string]interface{}{
-		"App Name":         "e-Channel Recycle Transaction",
-		"App Description":  "e-Channel Retransaction API Endpoint",
-		"App Version":      "1.4.2",
-		"App Author":       "Azmi Farhan",
-		"App Release Date": "13/10/2022 17:53",
+		"App Name":        "e-Channel Recycle Transaction",
+		"App Description": "e-Channel Retransaction API Endpoint",
+		"App Version":     "1.4.3",
+		"App Author":      "Azmi Farhan",
+		"App Updated At:": "14/10/2022 15:27",
 	}
 
 	httpio.Response(http.StatusOK, appInfo)
